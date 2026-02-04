d5723dcef51689c43955671f89e897ecc8c55e9f
Nito <tanhoangwibu@gmail.com>
2026-02-04 20:25:52 +0700

    feat: completed the connection with PostGIS

--- QL_tour/QL_tour/settings.py
+++ QL_tour/QL_tour/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.gis',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -42,7 +43,6 @@ INSTALLED_APPS = [
     'apps.tours',
     'apps.bookings',
     'apps.payments',
-    'django.contrib.gis',
 ]
 
 MIDDLEWARE = [
