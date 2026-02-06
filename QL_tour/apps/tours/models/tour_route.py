4d3e61209e1a188b34850be2f75dc694fbf3068c
Nito <tanhoangwibu@gmail.com>
2026-02-06 14:22:51 +0700

    feat: update database

--- QL_tour/apps/tours/models/tour_route.py
+++ QL_tour/apps/tours/models/tour_route.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.gis.db import models
 from .tours import Tour
+from .tour_stop import TourStop
 
 class TourRoute(models.Model):
-    tour = models.OneToOneField(
+    tour = models.ForeignKey(
         Tour,
         on_delete=models.CASCADE
+    )
+    stop = models.ForeignKey(
+        TourStop, 
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
     )
     route = models.LineStringField(
         geography=True,
--- QL_tour/apps/tours/models/tour_stop.py
+++ QL_tour/apps/tours/models/tour_stop.py
@@ -1,13 +1,7 @@
 from django.db import models
 from django.contrib.gis.db import models
-from .tours import Tour
 
 class TourStop(models.Model):
-    tour = models.ForeignKey(
-        Tour,
-        on_delete=models.CASCADE,
-        related_name="stops"
-    )
     name = models.CharField(max_length=255)
     location = models.PointField(geography=True, srid=4326)
     order = models.PositiveIntegerField()
